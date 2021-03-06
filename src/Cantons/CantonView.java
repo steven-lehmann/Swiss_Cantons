ac2b99ddfa3ec7718247b49a5db03997b55ec8d5
femin6jose <feminjose.kunnathuparambil@students.fhnw.ch>
2021-03-06 14:08:57 +0100

    Stylesheet hinzugefügt und MainView erstellt

--- src/Cantons/CantonView.java
+++ src/Cantons/CantonView.java
@@ -4,9 +4,13 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -17,42 +21,65 @@ public class CantonView {
 	
 	//elements in the GUI
 	protected ChoiceBox<String> cantons;
-	protected Button submitButton;
-	protected final Image ALLCANTONS = new Image("/cantons.png"); //TODO add new Picture
-	protected ImageView cantonView;
+	protected Button createButton, displayButton;
+	//protected final Image ALLCANTONS = new Image("/cantons.png");
+	//protected ImageView cantonView;
+	protected MenuBar menus = new MenuBar();
+	protected Menu menuHome = new Menu("Home");
+	
+	protected Label lbHeading, lbTeaser, lbNewCanton, lbViewCanton;
 	
 
 	public CantonView(Stage stage, CantonModel model) {
 		this.model = model;
 		this.stage = stage;
 		
+		//Home View
+		
 		BorderPane root = new BorderPane();
 		VBox center = new VBox();
+		GridPane buttonGrid = new GridPane();
 		
 		this.cantons = new ChoiceBox<String>();
-		this.submitButton = new Button("Submit");
-		this.cantonView = new ImageView(ALLCANTONS);
+		this.createButton = new Button("Neu");
+		this.displayButton = new Button("Anzeigen");
+		//this.cantonView = new ImageView(ALLCANTONS);
+		
+		
+		
+		lbHeading = new Label("Willkommen");
+		lbTeaser = new Label("Dies ist eine Übersicht der Kanton in der Schweiz ");
+		lbNewCanton = new Label("Kanton hinzufügen:");
+		lbViewCanton = new Label("Kanton anzeigen:");
+		
+		root.setTop(menus);
+		menus.getMenus().add(menuHome);
 		
 		cantons.getItems().addAll("Basel", "Bern", "Zürich");
 		cantons.setValue("Bern");
 		
 		
-		center.getChildren().addAll(this.cantons, this.submitButton);
+		buttonGrid.add(this.lbNewCanton, 0,0);
+		buttonGrid.add(this.createButton, 1,0);
+		buttonGrid.add(this.lbViewCanton, 0,1);
+		buttonGrid.add(this.cantons, 1,1);
+		buttonGrid.add(this.displayButton, 2,1);
 		
-		root.setTop(cantonView);
+		center.getChildren().addAll(this.lbHeading, this.lbTeaser, buttonGrid);
+		
 		root.setCenter(center);
 		
-		Scene scene = new Scene(root, 1200, 800);
-		//TODO Style sheet 
+		Scene scene = new Scene(root, 800, 700);
+		scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
 		stage.setScene(scene);
 		stage.setTitle("Kanton App");
 		
 		
 		//Some stylings 
-		center.setAlignment(Pos.CENTER);
-		cantonView.setFitHeight(481);
-		cantonView.setFitWidth(720);
-		root.setAlignment(cantonView, Pos.CENTER);
+		//center.setAlignment(Pos.CENTER);
+		//cantonView.setFitHeight(481);
+		//cantonView.setFitWidth(720);
+		//root.setAlignment(cantonView, Pos.CENTER);
 	}
 
 	public void start() {
