ffe640b0dbf92a07dafb97da34d946f07701aa3a
femin6jose <feminjose.kunnathuparambil@students.fhnw.ch>
2021-03-06 18:42:15 +0100

    Zweite View Ansicht für die Anzeige der Kantone erstellt

--- src/Cantons/CantonView.java
+++ src/Cantons/CantonView.java
@@ -1,6 +1,5 @@
 package Cantons;
 
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -24,8 +23,10 @@ public class CantonView {
 	//elements in the GUI
 	protected ChoiceBox<String> cantons;
 	protected Button createButton, displayButton, editButton, deletButton, submitButton;
+	
 	//protected final Image ALLCANTONS = new Image("/cantons.png");
 	//protected ImageView cantonView;
+	
 	protected MenuBar menus = new MenuBar();
 	protected Menu menuHome = new Menu("Home");
 	
@@ -34,6 +35,12 @@ public class CantonView {
 	
 	protected TextField txtName, txtSize, txtPopulation, txtAcronym, txtYear, 
 			txtLanguage, txtMainPlace, txtNeighbor, txtLink, txtAttractions;
+	
+	//protected final Image BLFLAG = new Image("/BLFlagge.png");
+	//protected ImageView cantonBLView;
+	
+	//protected final Image BLCARD = new Image("/BLKarte.png");
+	//protected ImageView cardBLView;
 	
 
 	public CantonView(Stage stage, CantonModel model) {
@@ -45,56 +52,129 @@ public class CantonView {
 		BorderPane root = new BorderPane();
 		VBox center = new VBox();
 		GridPane buttonGrid = new GridPane();
-		
+				
 		this.cantons = new ChoiceBox<String>();
 		this.createButton = new Button("Neu");
 		this.displayButton = new Button("Anzeigen");
 		//this.cantonView = new ImageView(ALLCANTONS);
-		
-		
-		
+				
+				
 		lbHeading = new Label("Willkommen");
 		lbTeaser = new Label("Dies ist eine Übersicht der Kanton in der Schweiz ");
 		lbNewCanton = new Label("Kanton hinzufügen:");
 		lbViewCanton = new Label("Kanton anzeigen:");
-		
+				
 		root.setTop(menus);
 		menus.getMenus().add(menuHome);
-		
+				
 		cantons.getItems().addAll("Basel", "Bern", "Zürich");
 		cantons.setValue("Bern");
-		
-		
-		buttonGrid.add(this.lbNewCanton, 0,0);
-		buttonGrid.add(this.createButton, 1,0);
-		buttonGrid.add(this.lbViewCanton, 0,1);
-		buttonGrid.add(this.cantons, 1,1);
-		buttonGrid.add(this.displayButton, 2,1);
-		
+				
+				
+		buttonGrid.add(this.lbNewCanton, 0, 0);
+		buttonGrid.add(this.createButton, 1, 0);
+		buttonGrid.add(this.lbViewCanton, 0, 1);
+		buttonGrid.add(this.cantons, 1, 1);
+		buttonGrid.add(this.displayButton, 2, 1);
+			
+				
 		center.getChildren().addAll(this.lbHeading, this.lbTeaser, buttonGrid);
-		
+				
+				
 		root.setCenter(center);
+		
 		
 		
 		// Kanton View
 		
-		BorderPane cantonView = new BorderPane();
-		HBox middle = new HBox();
-		VBox left = new VBox();
-		VBox right = new VBox();
-		GridPane cantonViewLeft = new GridPane();
-		GridPane cantonViewRight = new GridPane();
-		
-		
-		
+				BorderPane cantonView = new BorderPane();
+				HBox middle = new HBox();
+				VBox left = new VBox();
+				VBox right = new VBox();
+				GridPane cantonViewLeft = new GridPane();
+				GridPane cantonViewRight = new GridPane();
+				
+				//Linke Spalte
+				
+				lbName = new Label("Name: ");
+				lbAcronym = new Label("Kürzel: ");
+				lbMainPlace = new Label("Hauptort");
+				lbLanguage = new Label("Sprache: ");
+				lbYear = new Label("Beitrittsjahr: ");
+				
+				this.txtName = new TextField();
+				this.txtAcronym = new TextField();
+				this.txtMainPlace = new TextField();
+				this.txtLanguage = new TextField();
+				this.txtYear = new TextField();
+				
+				
+				cantonViewLeft.add(lbName, 0, 0);
+				cantonViewLeft.add(txtName, 1, 0);
+				cantonViewLeft.add(lbAcronym, 0, 1);
+				cantonViewLeft.add(txtAcronym, 1, 1);
+				cantonViewLeft.add(lbMainPlace, 0, 2);
+				cantonViewLeft.add(txtMainPlace, 1, 2);
+				cantonViewLeft.add(lbLanguage, 0, 3);
+				cantonViewLeft.add(txtLanguage, 1, 3);
+				cantonViewLeft.add(lbYear, 0, 4);
+				cantonViewLeft.add(txtYear, 1, 4);
+				//cantonViewLeft.add(cardBLView, 0, 5);
+				
+				left.getChildren().add(cantonViewLeft);
+				
+				//Rechte Spalte
+				
+				lbPopulation = new Label("Bevölkerung: ");
+				lbSize = new Label("Fläche: ");
+				lbAttractions = new Label("Top 3 Sehenswürdigkeiten");
+				lbNeighbor = new Label("Nachbarkantone");
+				lbLink = new Label("Link");
+				
+				this.txtPopulation = new TextField();
+				this.txtSize = new TextField();
+				this.txtAttractions = new TextField();
+				this.txtNeighbor = new TextField();
+				this.txtLink = new TextField();
+				
+				
+				
+				//cantonViewRight.add(cantonBLView, 0, 0);
+				cantonViewRight.add(lbPopulation, 0, 1);
+				cantonViewRight.add(txtPopulation, 1, 1);
+				cantonViewRight.add(lbSize, 0, 2);
+				cantonViewRight.add(txtSize, 1, 2);
+				cantonViewRight.add(lbAttractions, 0, 3);
+				cantonViewRight.add(txtAttractions, 1, 3);
+				cantonViewRight.add(lbNeighbor, 0, 4);
+				cantonViewRight.add(txtNeighbor, 1, 4);
+				cantonViewRight.add(lbLink, 0, 5);
+				cantonViewRight.add(txtLink, 1, 5);
+				
+				right.getChildren().add(cantonViewRight);
+				
+				//HBox füllen
+				
+				middle.getChildren().addAll(left, right);
+				
+				//BorderPane füllen
+				
+				cantonView.setTop(menus);
+				cantonView.setCenter(middle);
+
 		
 		//Set Scene
 		
-		Scene scene = new Scene(root, 800, 700);
+		Scene scene = new Scene(root, 1000, 800);
 		scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
 		stage.setScene(scene);
 		stage.setTitle("Kanton App");
 		
+		//cardBLView.setFitHeight(50);
+		//cardBLView.setFitWidth(70);
+		
+		//cantonBLView.setFitHeight(30);
+		//cantonBLView.setFitWidth(20);
 		
 		//Some stylings 
 		//center.setAlignment(Pos.CENTER);
