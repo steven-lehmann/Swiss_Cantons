d3f84b4968a44c71ae8cb2d4801605ad7d8f62a9
gosi333 <gowsigan.gowribalan@students.fhnw.ch>
2021-03-08 10:46:48 +0100

    methodeHead Model

--- src/Cantons/CantonModel.java
+++ src/Cantons/CantonModel.java
@@ -1,5 +1,23 @@
 package Cantons;
 
 public class CantonModel {
+	
+	public void displayCanton() {
+		
+	}
+	
+	public void crateCanton () {
+		
+	}
+	
+	public void editCanton() {
+		
+	}
+	
+	public void deleteCanton() {
+		
+	}
+	
+	
 
 }
